dc5b6774fb6b7cbcb187c45697b74470c65f16f7
Alex Zhang <azq4306@gmail.com>
2025-11-13 20:09:03 -0500

    more changes for aruco recognition of image

--- ArucoProc.py
+++ ArucoProc.py
@@ -5,7 +5,6 @@ import numpy as np
 
 # establish camera, arUco settings
 # cap = cv2.VideoCapture(1)
-img = cv2.imread("path/to/image.jpg")
 ground = cv2.createBackgroundSubtractorMOG2(history=200, varThreshold=50)
 
 arucoDict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
@@ -23,10 +22,14 @@ minArea = 800
 
 # if the camera is functional, set up the matrix of positions based on the camera’s range of vision
 while True:
-	ret, frame = img.read()
-	if not ret:
-		break
+	# ret, frame = cap.read()
+	# if not ret:
+	# 	break
 	
+	frame = cv2.imread("aruco_tag.png")
+	if frame is None:
+		print("Failed to load image")
+		# break
 	h, w = frame.shape[:2]
 	f = w
 	cameraMatrix = np.array([[f, 0, w/2], [0, f, h/2], [0, 0, 1]], dtype=np.float32)
@@ -35,12 +38,13 @@ while True:
 	# ArUco markers - establish the tag and identify it within the camera’s view
 	gray = cv2. cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	corners, ids, rejected = aruco.detectMarkers(gray, arucoDict, parameters=parameters)
-	
+	print(corners)
+	print(ids)
 	# if the ArUco tag is detected properly, determine the position of the object based on the tag’s communicated information + initial camera view matrix, convert from pixels to meters
 	if ids is not None:
 		aruco.drawDetectedMarkers(frame, corners, ids)
 		rvecs, tvecs, _objPoints = aruco.estimatePoseSingleMarkers(corners, markerLength, cameraMatrix, distCoeffs)
-		aruco.drawAxis(frame, cameraMatrix, distCoeffs, rvecs[0], tvecs[0], 0.05)
+		cv2.drawFrameAxes(frame, cameraMatrix, distCoeffs, rvecs[0], tvecs[0], 0.05)
 
 		xC, yC, zC = tvecs[0][0]
 		yNew = max(cHeightMax - zC, 0)
@@ -57,6 +61,7 @@ while True:
 	key = cv2.waitKey(10)
 	if key == 27:
 		break
+
 
 # cap.release()
 cv2.destroyAllWindows()
